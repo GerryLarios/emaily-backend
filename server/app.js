f7ce06d766902ee06aed6c8b8af854e75dd97a95
Gerardo Manuel Chávez Larios <gerardo.larios69@gmail.com>
2019-09-03 21:43:52 -0500

    [ADD] mongo connection

--- server/app.js
+++ server/app.js
@@ -1,16 +1,26 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://127.0.0.1:27017/emaily_test', { useNewUrlParser: true })
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection Error'));
+db.once('open', () => console.log('Successfully Connected'));
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
