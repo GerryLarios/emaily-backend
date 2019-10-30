ad526f3de90ec114c2a0b72c5904a76b7ec95357
Gerardo Manuel Chávez Larios <gerardo.larios69@gmail.com>
2019-10-29 23:49:31 -0600

    [FIX] react-redux dependecie

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
 import App from './components/App'
 
